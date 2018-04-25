0e7a888f5020b2a219947fa22b64372722b7aa05
BuildTools <unconfigured@null.spigotmc.org>
2018-04-25 21:07:24 +0200

    sigurds homework xD

--- Informationsteknologi eksamen/Bestil.php
+++ Informationsteknologi eksamen/Bestil.php
@@ -1,14 +1,9 @@
 <?php
-
-include 'connect.php';
-
-if (isset($_POST['submit'])) {
-	$email = $_POST['inputEmail'];
-	$password = $_POST['inputPassword'];
-  $remember = $_POST['remember-me'];
-}
-
-$mysqli->close();
+//if (session_status() == PHP_SESSION_NONE) {
+//  echo "<p> Log venligst ind: <br>
+//    <a href=\"log_ind.php\" class=\"btn btn-default btn-lg active\" role=\"button\">Gå til log in side</a>";
+//    exit;
+//}
 ?>
 <html lang="en">
 
@@ -27,7 +22,6 @@ $mysqli->close();
 
     <!-- Custom styles for this template -->
     <link href="css/business-frontpage.css" rel="stylesheet">
-
 </head>
 
 
@@ -42,52 +36,128 @@ $mysqli->close();
           <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="index.html">Hjem
+                <a class="nav-link" href="index.html">Bagere i nærheden
                   <span class="sr-only">(current)</span>
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="Om_os.html">Om os</a>
+                <a class="nav-link" href="index.html">Hjem</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="Bestil.html">Bestil</a>
+                <a class="nav-link" href="registrer.php">Bestil</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="Kontakt_os.html">Kontakt os</a>
+                <a class="nav-link" href="bestil.php">Kontakt os</a>
               </li>
             </ul>
           </div>
         </div>
       </nav>
+      <div class="container">
 
-
-
-      <div class="row justify-content-center"></div>
-          <div class="col-sm-6">
-            
-              <form class="form-signin">
-                <h2 class="form-signin-heading mt-4">Log venligst ind</h2>
-                <label for="inputEmail" class="sr-only">Email adresse</label>
-                <input type="email" id="inputEmail" class="form-control" placeholder="Email adresse" required autofocus>
-                <label for="inputPassword" class="sr-only">Adgangskode</label>
-                <input type="password" id="inputPassword" class="form-control" placeholder="Adgangskode" required>
-                <div class="checkbox">
-                  <label>
-                    <input type="checkbox" value="remember-me"> Husk mig
-                  </label>
-                </div>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">Log ind</button>
-              </form>
-        
-           </div>
+      <div class="row">
+        <div class="col-sm-8">
+          <h2 class="mt-4">Bestilling af varer</h2>
+          <p>Det er nu muligt at bestille dit brød hjemmefra!</p>
+          <p>Find de bedste tilbud først og så kan du bestille ved blot et klik.</p>
+          <p>
+          <div class="form-group">
+          <form method="post">
+          <label for="liste">Select list:</label>
+            <select class="form-control" id="liste" name="by">
+              <option value="Vejle">Vejle</option>
+              <option value="Jelling">Jelling</option>
+              <option value="Bredsten">Bredsten</option>
+              <option value="Give">Give</option>
+            </select>
+            <button class="btn btn-lg btn-primary btn-block" id="submit" type="submit" name="submit">Find bager</button>
+            </form>
+          </div> 
+          </p>
+          <h2>Nyheder fra bagerierne</h2>
         </div>
-
+        <div class="col-sm-4">
+          <h2 class="mt-4">Find din bager</h2>
+          <?php
+          if (!isset($_POST['submit'])) {
+            echo "
+              <address>
+                <strong>Bagere i nærheden</strong>
+                <br>Langenæs Bageri, Vejle
+                <br>Palles Bageri, Vejle
+                <br>Lagkagehuset, Vejle
+                <br>Ødsted Bageri, Vejle
+                <br>P.a. Andersen & Sønner, Vejle
+                <br>og mange flere
+              </address>";
+            }
+          if (isset($_POST['submit']) && $_POST['by'] == 'Vejle') {
+            echo "
+              <address>
+                <strong>Bagere i nærheden</strong>
+                <br>Langenæs Bageri, Vejle
+                <br>Palles Bageri, Vejle
+                <br>Lagkagehuset, Vejle
+                <br>Ødsted Bageri, Vejle
+                <br>P.a. Andersen & Sønner, Vejle
+                <br>og mange flere
+              </address>";
+              }
+            if (isset($_POST['submit']) && $_POST['by'] == 'Jelling') {
+              echo "
+                <address>
+                  <strong>Bagere i nærheden</strong>
+                  <br>SuperBrugsen, Jelling
+                  <br>Controlstone v/Niels Bager Bæk, Jelling
+                  <br>Hjertebageri, Vejle
+                  <br>Palles Bageri, Vejle
+                </address>";
+                }
+            if (isset($_POST['submit']) && $_POST['by'] == 'Bredsten') {
+              echo "
+                <address>
+                  <strong>Bagere i nærheden</strong>
+                  <br>Dagli'Brugsen, Bredsten
+                  <br>Bredsten Hallen's Cafeteria v/Klaus Bager, Bredsten
+                  <br>Ny Nørup Bageri, Ny Nørup
+                </address>";
+                }
+            if (isset($_POST['submit']) && $_POST['by'] == 'Give') {
+              echo "
+                <address>
+                  <strong>Bagere i nærheden</strong>
+                  <br>Nytorvs Bageri, Give
+                  <br>Superbrugsen, Give
+                </address>";
+                }                        
+          ?>
+        </div>
+      </div>
+      
+      <iframe
+        width="600"
+        height="450"
+        frameborder="0" style="border:0"
+        src="https://www.google.com/maps/embed/v1/search?key=AIzaSyAbIkMp7dCfdvunIoLwTC8zfAk7pCyYDhU 
+          &q=
+          <?php
+          if (!isset($_POST['submit'])) {
+            echo "Vejle+bager";
+          }
+          if (isset($_POST['submit']) && $_POST['by'] == 'Vejle' ) {
+            echo "Vejle+bager";
+          }
+          if (isset($_POST['submit']) && $_POST['by'] == 'Jelling' ) {
+            echo "Jelling+bager";
+          }
+          if (isset($_POST['submit']) && $_POST['by'] == 'Bredsten' ) {
+            echo "Bredsten+bager";
+          }
+          if (isset($_POST['submit']) && $_POST['by'] == 'Give' ) {
+            echo "Give+bager";
+          }
+          ?>"
+          allowfullscreen>
+      </iframe>
 </body>
-
-
-
-
-
-
-
 </html>
--- Informationsteknologi eksamen/log_ind.php
+++ Informationsteknologi eksamen/log_ind.php
@@ -1,36 +1,32 @@
 <?php
-include 'connect.php';
-session_start();
+    include 'connect.php';
+    session_start();
+    $username=$password="";
 
-if (isset($_POST['submit'])) {
-   // username and password sent from form 
-   
-   $myemail = mysqli_real_escape_string($db,$_POST['email']);
-   $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-   
-   $sql = "SELECT id FROM $table_ WHERE username = '$myemail' and passcode = '$mypassword'";
-   $result = mysqli_query($db,$sql);
-   $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-   $active = $row['active'];
-   
-   $count = mysqli_num_rows($result);
-   
-   // If result matched $myusername and $mypassword, table row must be 1 row
-     
-   if($count == 1) {
-      session_register("myusername");
-      $_SESSION['login_user'] = $myemail;
-      
-      header("location: welcome.php");
-   }else {
-      $error = "Dit log ind navn eller din adgangskode er forkert, prøv venligst igen";
-   }
-}
+
+    if (isset($_POST['submit'])) {
+      $username = $_POST['inputUsername'];
+	    $password = $_POST['inputPassword'];
+        
+    }
+    
+    if($username != ""){
+        $res = $mysqli->query("SELECT id, email, password FROM $table_5 WHERE email='$username'");
+        $row = mysqli_fetch_array($res);
+        $count = mysqli_num_rows($res); 
+    
+        if( $count == 1 && $row['password']==$password ) {
+         $_SESSION['user'] = $row['id'];
+         
+         header("Location: bestil.php");
+        } else {
+         echo "WRONG!!!";
+        }
+    }
+    echo $mysqli->error;
 ?>
-<html lang="en">
-
-<head>
-
+<html>
+  <head>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -48,7 +44,7 @@ if (isset($_POST['submit'])) {
 </head>
 
 
-<body>
+    <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container">
           <a class="navbar-brand" style="color: white;">Bager Bestillingen</a>
@@ -62,8 +58,8 @@ if (isset($_POST['submit'])) {
                 <a class="nav-link" href="index.html">Hjem
                 </a>
               </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="log_ind.php">Log ind</a>
+              <li class="nav-item">
+                <a class="nav-link active" href="log_ind.php">Log ind</a>
                 <span class="sr-only">(current)</span>
               </li>
               <li class="nav-item">
@@ -73,7 +69,6 @@ if (isset($_POST['submit'])) {
           </div>
         </div>
       </nav>
-
       <div class="row justify-content-center"></div>
           <div class="col-sm-6">
             
@@ -85,7 +80,7 @@ if (isset($_POST['submit'])) {
                 <input type="password" id="inputPassword" class="form-control" placeholder="Adgangskode" name="inputPassword" required>
                 <button class="btn btn-lg btn-primary btn-block" id="submit" type="submit" name="submit">Log ind</button>
               </form>
-              <p> Har du ikke en bruger? Så registrer her </p>
+              <p> Har du ikke en bruger, så registrer dig her:</p>
               <a href="log_ind.php" class="btn btn-default btn-lg active" role="button">Registrer</a>
            </div>
         </div>
